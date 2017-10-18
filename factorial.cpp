134496cb18bbadef247ecb41d280ca4169e188f0
Regi <32775617+re-0@users.noreply.github.com>
2017-10-18 22:08:02 +0100

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -5,11 +5,19 @@
 using namespace std;
 
 auto factorial(auto n){
+/********** Obsolete
     if(n < 1){
         return 1;
     } else {
         return n*factorial(n - 1);
     }
+************/
+    // Many thanks to @presariohg for showing me a more beautiful way to find n! ~
+long long result = 1;
+    for(int i = 1; i <= n; i++){
+        result *= i;
+    }
+return result;
 }
 
 int main()
